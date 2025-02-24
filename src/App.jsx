e1a527d92b79451285c06667d7b9ad7ae609c127
taybaa <shaikhtayba3@gmail.com>
2025-02-24 21:12:10 +0530

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-
+import Navigation from './Components/Navigation'
 const App = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">TailwindCSS is Working! 🎉</h1>
-      <button className="mt-5 px-6 py-3 bg-green-500 text-white rounded-lg hover:bg-green-600 transition">
-        Click Me
-      </button>
-    </div>
+    <>
+    <Navigation />
+    </>
   );
 };
 
--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import { FaUserCircle } from "react-icons/fa";
 function Navigation() {
   return (
-    <div>Navigation</div>
+    <nav className='border-2 text-black  h-[60px] w-full flex justify-between'>
+      <input type="text" placeholder="Enter text" class="border w-[300px] border-gray-400 p-2 rounded-lg h-10 ml-10 mt-2" />
+
+      <div className='mr-10 mt-2'><FaUserCircle size={40} /></div>
+    </nav>
   )
 }
 
