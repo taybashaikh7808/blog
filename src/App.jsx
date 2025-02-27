c1cd47b0663283ef8ee255adda148a14ee1fa7f8
taybaa <shaikhtayba3@gmail.com>
2025-02-27 23:36:48 +0530

    strted wrking on model and blogform

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import TrendsList from "./Components/TrendsList";
 import PeopleToFollow from "./Components/PeopleToFollow";
-import Navigation from "./Components/Navigation"
-import TopicsList from "./Components/TopicsList"
-import BlogContext from "./shared/BlogContext";
+import Navigation from "./Components/Navigation";
+import TopicsList from "./Components/TopicsList";
+import { BlogProvider } from "./shared/BlogContext"; // Import BlogProvider
+import blogs from "./db/trends";
+import Model from './Components/Model'
+import BlogForm from "./Components/BlogForm";
+
 function App() {
+  const [isModelOpen, setisModelOpen] = useState(false)
+  const [editing, setediting] = useState(null)
+
+  const openModelforNewblog = () =>{
+    setediting(null)
+    setisModelOpen(true)
+  }
+
+  const openModelforEdit = () =>{
+    setediting(blogs)
+    setisModelOpen(true)
+  }
   return (
-    <div>
-      <Navigation />
-      <BlogContext>
-        <button>Add Blog</button>
-    <div className="flex justify-end p-4">
-      <div className="w-[400px] flex flex-col gap-4">
-        <PeopleToFollow />
-        <TrendsList />
-        <TopicsList />
+    <BlogProvider>
+      <div className="flex flex-col min-h-screen">
+        {/* Navigation Bar */}
+        <Navigation />
+
+        {/* Centered Add Blog Button */}
+        <div className="flex flex-col items-center justify-center mt-4">
+          <button onClick={openModelforNewblog} className="border-2 border-black bg-black text-white w-28 h-10 flex justify-center items-center">
+            Add New Blog
+          </button>
+          {isModelOpen && <Model>
+            <BlogForm /></Model>}
+        </div>
+
+        {/* Sidebar Section - Move Up */}
+        <div className="absolute top-[80px] right-4 w-[400px] flex flex-col gap-4">
+          <PeopleToFollow /> {/* This component is now at the top */}
+          <TrendsList />
+          <TopicsList />
+        </div>
       </div>
-    </div>
-    </BlogContext>
-    </div>
+    </BlogProvider>
   );
 }
 
--- src/Components/Model.jsx
+++ src/Components/Model.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-function Model() {
-  return (
-    <div>Model</div>
+function Model({children,onClose}) {
+  return ( 
+    <div className="fixed bg-black bg-opacity-50">
+      <div className="bg-white">{children}
+        <div className="border-2" onClick={onClose}></div>
+      </div>
+    </div>
   )
 }
 
