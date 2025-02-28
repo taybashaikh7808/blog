1342e4ad90f60370dc171d309842fcc3ed0e6ea3
taybaa <shaikhtayba3@gmail.com>
2025-02-28 23:44:51 +0530

    Blogform created usestates

--- src/Components/BlogForm.jsx
+++ src/Components/BlogForm.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function BlogForm() {
+function BlogForm({existingBlog,onClose}) {
+  const {addBlog,updateBlog} = useBlog()
+  const [title, settitle] = useState(existingBlog?.title || " ")
+  const [description, setdescription] = useState(existingBlog?.description || " ")
+  const [image, setimage] = useState(existingBlog?.image || "")
+  const [time, settime] = useState(existingBlog?.time || " ")
+  useEffect(() => {
+    if(existingBlog){
+      settitle(existingBlog.title)
+      setdescription(existingBlog.description)
+      setimage(existingBlog.image)
+      settime(existingBlog.time)
+    }
+  }, [existingBlog])
+  
   return (
     <div>BlogForm</div>
   )
